69e5d6d3b5faeac2f0402e4a8f1da9e4ed6a059a
Thomas Grefsrud <thomas.grefsrud@payex.com>
2019-07-03 12:27:15 +0200

    VASEVC-3979 : merged and added some files

---
+++ frontend/src/views/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <CardList/>
+    <p> Selected api is: {{selectedApi}}</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CardList from '@/components/CardList.vue'
+
+export default {
+  name: 'Card',
+  components: {
+    CardList
+  },
+  props: {
+    'selectedApi': String
+  }
+}
+</script>
