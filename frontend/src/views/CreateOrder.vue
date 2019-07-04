86275504c035f9471e43ef1a107da7ad39f7e79b
Thomas Grefsrud <thomas.grefsrud@payex.com>
2019-07-04 14:00:14 +0200

    VASEVC-3979 : merged changes and prettified everything!

--- frontend/src/views/CreateOrder.vue
+++ frontend/src/views/CreateOrder.vue
@@ -5,17 +5,22 @@
       <main class="mainContainer">
         <div v-if="customer && customerType === 'private'">
           <label for="name">Customer name</label>
-          <div class="form-control" id="name">{{customer.customerFirstName + ' ' + customer.customerLastName}}</div>
+          <div
+            class="form-control"
+            id="name"
+          >{{customer.customerFirstName + ' ' + customer.customerLastName}}</div>
         </div>
         <AddCustomerInfo
           @add-customer-successful="updateCustomerData"
           :message="message"
           style="margin-bottom: 2rem"
         />
-        <AddProduct :productMessage="productMessage" style="margin-bottom: 2rem"/>
+        <AddProduct :productMessage="productMessage" style="margin-bottom: 2rem" />
         <label for="description">Order Description</label>
         <div class="input-group">
-          <span class="input-group-addon"><i class="material-icons">description</i></span>
+          <span class="input-group-addon">
+            <i class="material-icons">description</i>
+          </span>
           <textarea
             class="form-control"
             id="description"
@@ -25,53 +30,54 @@
           ></textarea>
         </div>
         <div class="row">
-      <label class="col" for="amount">Amount</label>
-      <label class="col-md-2" for="operation">Currency</label>
-    </div>
+          <label class="col" for="amount">Amount</label>
+          <label class="col-md-2" for="operation">Currency</label>
+        </div>
         <div class="input-group amountSize">
-          <span class="input-group-addon"><i class="material-icons">monetization_on</i></span>
-          <input
-            class="form-control order-input"
-            id="amount"
-            v-model="initAmount"
-          >
+          <span class="input-group-addon">
+            <i class="material-icons">monetization_on</i>
+          </span>
+          <input class="form-control order-input" id="amount" v-model="initAmount" />
           <span class="input-group-addon">,</span>
-          <input
-            class="form-control col-md-1 order-input"
-            v-model="initAmountCents"
-          >
-        <span class="input-group-addon"><i class="material-icons">attach_money</i></span>
-        <select id="operation" class="form-control col-md-2" v-model="paymentRequest.currency">
-          <option disabled value="">Select one</option>
-          <option>NOK</option>
-          <option>SEK</option>
-          <option>DKK</option>
-          <option>EUR</option>
-        </select>
-    </div>
-    <div class="form-group">
-      <div class="row">
-        <label class="col" for="merchant">Merchant</label>
-        <label class="col" for="invoice_type">Payment method</label>
-      </div>
-      <div class="input-group">
-            <span class="input-group-addon"><i class="material-icons">store_mall_directory</i></span>
+          <input class="form-control col-md-1 order-input" v-model="initAmountCents" />
+          <span class="input-group-addon">
+            <i class="material-icons">attach_money</i>
+          </span>
+          <select id="operation" class="form-control col-md-2" v-model="paymentRequest.currency">
+            <option disabled value>Select one</option>
+            <option>NOK</option>
+            <option>SEK</option>
+            <option>DKK</option>
+            <option>EUR</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <div class="row">
+            <label class="col" for="merchant">Merchant</label>
+            <label class="col" for="invoice_type">Payment method</label>
+          </div>
+          <div class="input-group">
+            <span class="input-group-addon">
+              <i class="material-icons">store_mall_directory</i>
+            </span>
             <select id="merchant" class="form-control" v-model="paymentRequest.merchant">
               <option disabled :value="{}">Please select one</option>
-              <option v-for="merchant in merchantList" :key="merchant.agreementId" v-bind:value="merchant">
-                {{merchant.merchantName}}
-              </option>
-        </select>
-        <select id="invoice-type" class="form-control" v-model="paymentRequest.paymentMethods">
-          <option disabled value="">Select one</option>
-          <option value="ALL">All</option>
-          <option value="INVOICE">Invoice</option>
-          <option value="ONLINE">Online</option>
+              <option
+                v-for="merchant in merchantList"
+                :key="merchant.agreementId"
+                v-bind:value="merchant"
+              >{{merchant.merchantName}}</option>
+            </select>
+            <select id="invoice-type" class="form-control" v-model="paymentRequest.paymentMethods">
+              <option disabled value>Select one</option>
+              <option value="ALL">All</option>
+              <option value="INVOICE">Invoice</option>
+              <option value="ONLINE">Online</option>
             </select>
           </div>
         </div>
         <div v-if="customer" class="checkbox">
-          <input type="checkbox" id="shipping" v-model="copyShippingInfo"/>
+          <input type="checkbox" id="shipping" v-model="copyShippingInfo" />
           <label for="shipping">Ship to billing address?</label>
         </div>
         <h6 style="margin-top: 2rem">Shipping Address Information</h6>
@@ -80,9 +86,9 @@
           <label class="col" for="shippingStreetAddress">Street Address</label>
         </div>
         <div class="input-group">
-        <span class="input-group-addon">
-          <i class="material-icons">account_box</i>
-        </span>
+          <span class="input-group-addon">
+            <i class="material-icons">account_box</i>
+          </span>
           <input
             type="text"
             class="form-control"
@@ -92,8 +98,8 @@
           />
           <div class="col-md-1"></div>
           <span class="input-group-addon">
-          <i class="material-icons">place</i>
-        </span>
+            <i class="material-icons">place</i>
+          </span>
           <input
             type="text"
             class="form-control"
@@ -108,9 +114,9 @@
           <label for="shippingCountryCode" class="col-md-3">Country</label>
         </div>
         <div class="input-group">
-        <span class="input-group-addon">
-          <i class="material-icons">location_city</i>
-        </span>
+          <span class="input-group-addon">
+            <i class="material-icons">location_city</i>
+          </span>
           <input
             type="text"
             class="form-control col-md-2"
@@ -138,117 +144,137 @@
           type="button"
           v-on:click="createOrder"
           style="margin-top: 3rem"
-        >Create order
-        </button>
+        >Create order</button>
       </main>
     </div>
   </div>
 </template>
 
 <script>
-  import AddCustomerInfo from '@/components/AddCustomerInfo.vue'
-  import AddProduct from '@/components/AddProduct.vue'
-  import { merchantService, multipayService } from '@/components/rest-resource.js'
-  import { toastError } from '@/utils/creditcard-util'
-  const uuidV4 = require('uuid/v4')
+import AddCustomerInfo from '@/components/AddCustomerInfo.vue';
+import AddProduct from '@/components/AddProduct.vue';
+import {
+  merchantService,
+  multipayService
+} from '@/components/rest-resource.js';
+import { toastError } from '@/utils/creditcard-util';
+const uuidV4 = require('uuid/v4');
 
-  export default {
-    name: 'CreateOrder',
-    components: {
-      AddCustomerInfo,
-      AddProduct
-    },
-    data() {
-      return {
-        paymentRequest: {
-          additionalData: '',
-          amount: 0,
-          corporateCustomerIdentifier: {},
-          currency: '',
-          privateCustomerIdentifier: {},
-          description: '',
-          merchant: {},
-          paymentContractId: '',
-          paymentExpireDateTime: '',
-          paymentMethods: '', // ALL, INVOICE, ONLINE
-          paymentOrderRef: '',
-          paymentTransactionRef: '',
-          paymentTransmissionDateTime: '',
-          preliminaryInvoiceFee: 0.0,
-          preliminaryInvoiceTax: 0.0,
-          products: [],
-          repeat: false,
-          shippingInformation: {},
-          stan: '',
-        },
-        customerType: '',
-        customer: null,
-        message: 'Add customer info',
-        productMessage: 'Add product',
+export default {
+  name: 'CreateOrder',
+  components: {
+    AddCustomerInfo,
+    AddProduct
+  },
+  data() {
+    return {
+      paymentRequest: {
+        additionalData: '',
+        amount: 0,
+        corporateCustomerIdentifier: {},
+        currency: '',
+        privateCustomerIdentifier: {},
+        description: '',
+        merchant: {},
+        paymentContractId: '',
+        paymentExpireDateTime: '',
+        paymentMethods: '', // ALL, INVOICE, ONLINE
+        paymentOrderRef: '',
+        paymentTransactionRef: '',
+        paymentTransmissionDateTime: '',
+        preliminaryInvoiceFee: 0.0,
+        preliminaryInvoiceTax: 0.0,
+        products: [],
+        repeat: false,
+        shippingInformation: {},
+        stan: ''
+      },
+      customerType: '',
+      customer: null,
+      message: 'Add customer info',
+      productMessage: 'Add product',
       initAmount: 0,
       initAmountCents: 0,
       copyShippingInfo: false,
-      merchantList: [],
-    }
+      merchantList: []
+    };
   },
-  mounted () {
+  mounted() {
     this.listMerchants();
   },
   methods: {
-    updateCustomerData (customer, type) {
+    updateCustomerData(customer, type) {
       if (type === 'corporate') {
-        this.paymentRequest.corporateCustomerIdentifier = customer
-      } else this.paymentRequest.privateCustomerIdentifier = customer
-      this.customerType = type
-      this.customer = customer
-      this.message = 'Edit customer info'
+        this.paymentRequest.corporateCustomerIdentifier = customer;
+      } else this.paymentRequest.privateCustomerIdentifier = customer;
+      this.customerType = type;
+      this.customer = customer;
+      this.message = 'Edit customer info';
     },
-    listMerchants () {
+    listMerchants() {
       merchantService.listMerchants().then(res => {
-        this.merchantList = res.data
-      })
+        this.merchantList = res.data;
+      });
     },
-    createOrder () {
-      this.paymentRequest.paymentContractId = uuidV4()
-      this.paymentRequest.paymentOrderRef = uuidV4()
-      this.paymentRequest.paymentTransmissionDateTime = new Date().toLocaleString('en-US', { hour12: false })
+    createOrder() {
+      this.paymentRequest.paymentContractId = uuidV4();
+      this.paymentRequest.paymentOrderRef = uuidV4();
+      this.paymentRequest.paymentTransmissionDateTime = new Date().toLocaleString(
+        'en-US',
+        { hour12: false }
+      );
 
       //send to backend
-      multipayService.createOrder(this.paymentRequest).then(res => {
-        px.toast({ html: 'Successfully created new order!' })
-        this.$root.$emit('order-create-event', res.data)
-      }).catch((error) => {
-        toastError(error)
-      })
+      multipayService
+        .createOrder(this.paymentRequest)
+        .then(res => {
+          px.toast({ html: 'Successfully created new order!' });
+          this.$root.$emit('order-create-event', res.data);
+        })
+        .catch(error => {
+          toastError(error);
+        });
     }
   },
   watch: {
-    initAmount () {
-      this.paymentRequest.amount = parseInt(this.initAmount) * 100 + parseInt(this.initAmountCents)
+    initAmount() {
+      this.paymentRequest.amount =
+        parseInt(this.initAmount) * 100 + parseInt(this.initAmountCents);
     },
-    initAmountCents () {
-      this.paymentRequest.amount = parseInt(this.initAmount) * 100 + parseInt(this.initAmountCents)
+    initAmountCents() {
+      this.paymentRequest.amount =
+        parseInt(this.initAmount) * 100 + parseInt(this.initAmountCents);
     },
-    copyShippingInfo () {
-      this.paymentRequest.shippingInformation.shippingAddressee     = this.paymentRequest[this.customerType + 'CustomerIdentifier'].address.billingAddressee
-      this.paymentRequest.shippingInformation.shippingCity          = this.paymentRequest[this.customerType + 'CustomerIdentifier'].address.billingCity
-      this.paymentRequest.shippingInformation.shippingCoAddress     = this.paymentRequest[this.customerType + 'CustomerIdentifier'].address.billingCoAddress
-      this.paymentRequest.shippingInformation.shippingCountryCode   = this.paymentRequest[this.customerType + 'CustomerIdentifier'].address.billingCountryCode
-      this.paymentRequest.shippingInformation.shippingPostalCode    = this.paymentRequest[this.customerType + 'CustomerIdentifier'].address.billingPostalCode
-      this.paymentRequest.shippingInformation.shippingStreetAddress = this.paymentRequest[this.customerType + 'CustomerIdentifier'].address.billingStreetAddress
+    copyShippingInfo() {
+      this.paymentRequest.shippingInformation.shippingAddressee = this.paymentRequest[
+        this.customerType + 'CustomerIdentifier'
+      ].address.billingAddressee;
+      this.paymentRequest.shippingInformation.shippingCity = this.paymentRequest[
+        this.customerType + 'CustomerIdentifier'
+      ].address.billingCity;
+      this.paymentRequest.shippingInformation.shippingCoAddress = this.paymentRequest[
+        this.customerType + 'CustomerIdentifier'
+      ].address.billingCoAddress;
+      this.paymentRequest.shippingInformation.shippingCountryCode = this.paymentRequest[
+        this.customerType + 'CustomerIdentifier'
+      ].address.billingCountryCode;
+      this.paymentRequest.shippingInformation.shippingPostalCode = this.paymentRequest[
+        this.customerType + 'CustomerIdentifier'
+      ].address.billingPostalCode;
+      this.paymentRequest.shippingInformation.shippingStreetAddress = this.paymentRequest[
+        this.customerType + 'CustomerIdentifier'
+      ].address.billingStreetAddress;
     }
   }
-
-  }
+};
 </script>
 
 <style>
+.mainContainer {
+  margin: 0 auto;
+}
 
-  .mainContainer {
-    margin: 0 auto;
-  }
-
-  .order-input {
-    text-align: right;
-  }
+.order-input {
+  text-align: right;
+}
 </style>
